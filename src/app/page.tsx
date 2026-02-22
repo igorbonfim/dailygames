b766f6f3aa2d3c634ef28f84f47cbae6253c39b3
Igor Bonfim <igorbonfim88@gmail.com>
2026-02-21 23:03:22 -0300

    Criando página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
+import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/game";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+async function getDailyGame() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+    );
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
+export default async function Home() {
+  const dailyGame: GameProps = await getDailyGame();
+
   return (
-    <main className="flex">
-      <h1>TESTE</h1>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo exclusivo para você
+        </h1>
+        <Link href={`/game/${dailyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image
+              src={dailyGame.image_url}
+              alt={dailyGame.title}
+              priority={true}
+              quality={100}
+              width={100}
+              height={100}
+            />
+          </section>
+        </Link>
+      </Container>
     </main>
   );
 }
