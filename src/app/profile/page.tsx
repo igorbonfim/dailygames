0b7fbad98a4f04bc0fdc0c3dd07725f57c4c02e6
Igor Bonfim <igorbonfim88@gmail.com>
2026-03-01 23:06:24 -0300

    Finalização da página perfil

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,13 @@ import { Container } from "@/components/container";
 import Image from "next/image";
 import userImg from "../../../public/user.png";
 import { FaShareAlt } from "react-icons/fa";
+import { FavoriteCard } from "./components/favorite";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Meu Perfil - Daily Games sua plataforma de jogos",
+  description: "Perfil GameDev!",
+};
 
 export default function Profile() {
   return (
@@ -24,6 +31,18 @@ export default function Profile() {
             <button className="bg-gray-700 px-4 py-3 rounded-lg">
               <FaShareAlt size={24} color="#FFF" />
             </button>
+          </div>
+        </section>
+
+        <section className="flex flex-wrap gap-5 flex-col md:flex-row">
+          <div className="grow flex-wrap">
+            <FavoriteCard />
+          </div>
+          <div className="grow flex-wrap">
+            <FavoriteCard />
+          </div>
+          <div className="grow flex-wrap">
+            <FavoriteCard />
           </div>
         </section>
       </Container>
